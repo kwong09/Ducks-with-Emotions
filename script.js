0eae41e36c46d1a08e000169282c93481dbaa2f6
kwong09 <wongkayla09@gmail.com>
2024-07-01 05:47:46 +0000

    20240630 Duck Graphics, Party Music, and Mouse Click

--- script.js
+++ script.js
@@ -4,26 +4,49 @@ let happyButton, madButton, sadButton, partyButton;
 let titleText, instructionsText;
 let buttonWidth, buttonHeight;
 let screen = 1;
+let happyDuckAni, sadDuckAni, madDuckAni, partyDuckAni;
+let font;
+let mouseClick, partyMusic;
 
 function preload() {
-  
+  //loads duck animations
+  happyDuckAni = loadAni("assets/happyDuckAnimation_frame1.png", "assets/happyDuckAnimation_frame2.png");
+  sadDuckAni = loadAni("assets/sadDuckAnimation_frame7.png", "assets/sadDuckAnimation_frame6.png", "assets/sadDuckAnimation_frame5.png", "assets/sadDuckAnimation_frame4.png", "assets/sadDuckAnimation_frame3.png", "assets/sadDuckAnimation_frame2.png", "assets/sadDuckAnimation_frame1.png");
+  madDuckAni = loadAni("assets/madDuckAnimation_frame1.png", "assets/madDuckAnimation_frame2.png", "assets/madDuckAnimation_frame3.png", "assets/madDuckAnimation_frame4.png");
+  partyDuckAni = loadAni("assets/partyDuckAnimation_frame1.png", "assets/partyDuckAnimation_frame2.png", "assets/partyDuckAnimation_frame3.png");
+
+  //loads font
+  font = loadFont("assets/slkscre.ttf");
+
+  //loads audio
+  mouseClick = loadSound("assets/mouseClickSound.mp3");
+  partyMusic = loadSound("assets/pixelPartyMusic.mp3");
 }
 
 //setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  makeButtons();
   background("lightyellow");
+  makeButtons();
+
+  //makes font pixelated
+  textFont(font);
+
+  //animation
+  happyDuckAni.frameDelay = 10;
+  sadDuckAni.frameDelay = 7;
+  madDuckAni.frameDelay = 4;
+  partyDuckAni.frameDelay = 8;
 
   //creates ducks with emotions
-  happyWhiteDuck = new Sprite (-100, -100, 60, 60, 'k');
-  happyWhiteDuck.color = 'yellow';
-  madWhiteDuck = new Sprite (-100, -100, 60, 60, 'k');
-  madWhiteDuck.color = 'red';
-  sadWhiteDuck = new Sprite (-100, -100, 60, 60, 'k');
-  sadWhiteDuck.color = 'blue';
-  partyWhiteDuck = new Sprite (-100, -100, 60, 60, 'k');
-  partyWhiteDuck.color = 'purple';
+  happyWhiteDuck = new Sprite (-100, -100, 80, 80, 'k');
+  happyWhiteDuck.addAni(happyDuckAni);
+  madWhiteDuck = new Sprite (-100, -100, 80, 80, 'k');
+  madWhiteDuck.addAni(madDuckAni);
+  sadWhiteDuck = new Sprite (-100, -100, 80, 80, 'k');
+  sadWhiteDuck.addAni(sadDuckAni);
+  partyWhiteDuck = new Sprite (-100, -100, 80, 80, 'k');
+  partyWhiteDuck.addAni(partyDuckAni);
 
 
 }
@@ -33,33 +56,45 @@ function draw() {
   //checking screens
   if (screen == 1) {
     happyScreen();
-    background("lightyellow");
+    backgroundText();
   }
   
   if (screen == 2) {
     madScreen();
+    backgroundText();
   }
   
   if (screen == 3) {
     sadScreen();
+    backgroundText();
   }
   
   if (screen == 4) {
     partyScreen();
+    backgroundText();
   }
 
   //pressing buttons
   if (happyButton.mouse.presses()) {
+    mouseClick.play();
     screen = 1;
+    partyMusic.stop();
   }
   if (madButton.mouse.presses()) {
+    mouseClick.play();
     screen = 2;
+    partyMusic.stop();
   }
   if (sadButton.mouse.presses()) {
+    mouseClick.play();
     screen = 3;
+    partyMusic.stop();
   }
   if (partyButton.mouse.presses()) {
+    mouseClick.play();
     screen = 4;
+    partyMusic.play();
+    partyMusic.loop();
   }
 }
 
@@ -96,12 +131,12 @@ function partyScreen() {
 }
 
 function makeButtons() {
+  textSize(20);
 
   buttonWidth = windowWidth/6;
   buttonHeight = windowHeight/13;
   
   //makes buttons at bottom to change emotions
-  textSize(20);
 
   happyButton = new Sprite(windowWidth/5, windowHeight - windowHeight/8, buttonWidth, buttonHeight, 'k');
   happyButton.color = "lightyellow";
@@ -118,4 +153,10 @@ function makeButtons() {
   partyButton = new Sprite(windowWidth/5 * 4, windowHeight - windowHeight/8, buttonWidth, buttonHeight, 'k');
   partyButton.color = "plum";
   partyButton.text = "Party";
+}
+
+function backgroundText() {
+  //text above buttons
+  textAlign(CENTER);
+  text('How do you feel today?', windowWidth/2, windowHeight - windowHeight/4);
 }
