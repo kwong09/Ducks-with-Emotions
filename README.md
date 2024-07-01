bfb07bb8c0d968c92f326d786de76d34b66bc740
kwong09 <wongkayla09@gmail.com>
2024-07-01 19:04:33 +0000

    20240701 Button Highlight, Resize Ducks, All Music, Advice Text, and Mute Button

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Ducks with Emotions
-Choose from the four buttons at the bottom of the screen to choose how you are feeling today. Music for each feeling is provided in hopes to make you feel happier when you are feeling down! It's also fun to see a little duck on your screen so that you can relate to your duck's emotions.
+Choose from the four buttons at the bottom of the screen to choose how you are feeling today. Music for each feeling is provided in hopes to make you feel happier when you are feeling down! It's also fun to see a little duck on your screen so that you can relate to your duck's emotions. For people who are sensitive to sound, I included a mute button for you to mute the music. Some text above the ducks gives your reassurance for how you are feeling!
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //variables
 let happyWhiteDuck, madWhiteDuck, sadWhiteDuck, partyWhiteDuck;
-let happyButton, madButton, sadButton, partyButton;
+let happyButton, madButton, sadButton, partyButton, muteButton, happyButtonHighlight, madButtonHighlight, sadButtonHighlight, partyButtonHighlight, isMuted = false;
 let titleText, instructionsText;
 let buttonWidth, buttonHeight;
 let screen = 1;
 let happyDuckAni, sadDuckAni, madDuckAni, partyDuckAni;
 let font;
-let mouseClick, partyMusic;
+let mouseClick, partyMusic, happyMusic, sadMusic, madMusic;
+let topText;
 
 function preload() {
   //loads duck animations
@@ -21,6 +22,9 @@ function preload() {
   //loads audio
   mouseClick = loadSound("assets/mouseClickSound.mp3");
   partyMusic = loadSound("assets/pixelPartyMusic.mp3");
+  happyMusic = loadSound("assets/happyGameMusic.mp3");
+  madMusic = loadSound("assets/madGameMusic.mp3");
+  sadMusic = loadSound("assets/sadGameMusic.mp3");
 }
 
 //setup
@@ -28,6 +32,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background("lightyellow");
   makeButtons();
+
+  happyDuckAni.scale = 2;
+  madDuckAni.scale = 2;
+  sadDuckAni.scale = 2;
+  partyDuckAni.scale = 2;
 
   //makes font pixelated
   textFont(font);
@@ -57,61 +66,125 @@ function draw() {
   if (screen == 1) {
     happyScreen();
     backgroundText();
+    textAlign(CENTER);
+    textSize(20);
+    text(topText, windowWidth/2, windowHeight/8);
+    topText = "Hooray! I hope your day \ncontinues to be awesome!";
   }
   
   if (screen == 2) {
     madScreen();
     backgroundText();
+    textAlign(CENTER);
+    textSize(20);
+    text(topText, windowWidth/2, windowHeight/8);
+    topText = "Being angry is natural for \nhumans to experience. But \ndon't let it control you";
   }
   
   if (screen == 3) {
     sadScreen();
     backgroundText();
+    textAlign(CENTER);
+    textSize(20);
+    text(topText, windowWidth/2, windowHeight/8);
+    topText = "It's okay, we all cry sometimes";
   }
   
   if (screen == 4) {
     partyScreen();
-    backgroundText();
+    backgroundText()
+    textAlign(CENTER);
+    textSize(20);
+    text(topText, windowWidth/2, windowHeight/8);;
+    topText = "PARTY PARTY PARTY!!";
   }
 
   //pressing buttons
   if (happyButton.mouse.presses()) {
     mouseClick.play();
     screen = 1;
-    partyMusic.stop();
+    if (!isMuted) {
+      happyMusic.play();
+      happyMusic.loop();
+      sadMusic.stop();
+      madMusic.stop();
+      partyMusic.stop();
+    }
   }
   if (madButton.mouse.presses()) {
     mouseClick.play();
     screen = 2;
-    partyMusic.stop();
+    if (!isMuted) {
+      happyMusic.stop();
+      sadMusic.stop();
+      madMusic.play();
+      madMusic.loop();
+      partyMusic.stop();
+    }
   }
   if (sadButton.mouse.presses()) {
     mouseClick.play();
     screen = 3;
-    partyMusic.stop();
+    if (!isMuted) {
+      happyMusic.stop();
+      sadMusic.play();
+      sadMusic.loop();
+      madMusic.stop();
+      partyMusic.stop();
+    }
   }
   if (partyButton.mouse.presses()) {
     mouseClick.play();
     screen = 4;
-    partyMusic.play();
-    partyMusic.loop();
+    if (!isMuted) {
+      happyMusic.stop();
+      sadMusic.stop();
+      madMusic.stop();
+      partyMusic.play();
+      partyMusic.loop();
+    }
+  }
+
+  if (isMuted == true) {
+    muteMusic();
+  }
+
+  //mutes or unmutes music
+  if (muteButton.mouse.presses()) {
+    if (!isMuted) {
+      isMuted = true;
+      muteButton.text = "Unmute";
+      muteButton.color = 'gray';
+    } else if (isMuted) {
+      isMuted = false;
+      muteButton.text = "Mute";
+      muteButton.color = 'white';
+    }
   }
 }
 
 function happyScreen() {
-  background("lightyellow");
+  background("#ffe957");
   happyWhiteDuck.pos = {x: windowWidth/2, y: windowHeight/2};
   madWhiteDuck.pos = {x: -100, y: -100};
   sadWhiteDuck.pos = {x: -100, y: -100};
   partyWhiteDuck.pos = {x: -100, y: -100};
+  happyButtonHighlight.pos = {x: windowWidth/5, y: windowHeight - windowHeight/8};
+  madButtonHighlight.pos = {x: -100, y: -100};
+  sadButtonHighlight.pos = {x: -100, y: -100};
+  partyButtonHighlight.pos = {x: -100, y: -100};
 }
 
 function madScreen() {
-  background("red");
+  background("#ed544c");
   madWhiteDuck.pos = {x: windowWidth/2, y: windowHeight/2};
   happyWhiteDuck.pos = {x: -100, y: -100};
   sadWhiteDuck.pos = {x: -100, y: -100};
   partyWhiteDuck.pos = {x: -100, y: -100};
+  madButtonHighlight.pos = {x: windowWidth/5*2, y: windowHeight - windowHeight/8};
+  happyButtonHighlight.pos = {x: -100, y: -100};
+  sadButtonHighlight.pos = {x: -100, y: -100};
+  partyButtonHighlight.pos = {x: -100, y: -100};
 }
 
 function sadScreen() {
@@ -120,30 +193,51 @@ function sadScreen() {
   happyWhiteDuck.pos = {x: -100, y: -100};
   madWhiteDuck.pos = {x: -100, y: -100};
   partyWhiteDuck.pos = {x: -100, y: -100};
+  sadButtonHighlight.pos = {x: windowWidth/5*3, y: windowHeight - windowHeight/8};
+  madButtonHighlight.pos = {x: -100, y: -100};
+  happyButtonHighlight.pos = {x: -100, y: -100};
+  partyButtonHighlight.pos = {x: -100, y: -100};
 }
 
 function partyScreen() {
-  background("plum");
+  background("#eec7ff");
   partyWhiteDuck.pos = {x: windowWidth/2, y: windowHeight/2};
   happyWhiteDuck.pos = {x: -100, y: -100};
   madWhiteDuck.pos = {x: -100, y: -100};
   sadWhiteDuck.pos = {x: -100, y: -100};
+  partyButtonHighlight.pos = {x: windowWidth/5*4, y: windowHeight - windowHeight/8};
+  madButtonHighlight.pos = {x: -100, y: -100};
+  sadButtonHighlight.pos = {x: -100, y: -100};
+  happyButtonHighlight.pos = {x: -100, y: -100};
 }
 
 function makeButtons() {
+
   textSize(20);
 
   buttonWidth = windowWidth/6;
   buttonHeight = windowHeight/13;
+
+  //makes border around buttons
+  happyButtonHighlight = new Sprite (-100, -100, buttonWidth + 10, buttonHeight + 10, 'k')
+  happyButtonHighlight.color = 'white';
+
+  madButtonHighlight = new Sprite (-100, -100, buttonWidth + 10, buttonHeight + 10, 'k')
+  madButtonHighlight.color = 'white';
+
+  sadButtonHighlight = new Sprite (-100, -100, buttonWidth + 10, buttonHeight + 10, 'k')
+  sadButtonHighlight.color = 'white';
+
+  partyButtonHighlight = new Sprite (-100, -100, buttonWidth + 10, buttonHeight + 10, 'k')
+  partyButtonHighlight.color = 'white';
   
   //makes buttons at bottom to change emotions
-
   happyButton = new Sprite(windowWidth/5, windowHeight - windowHeight/8, buttonWidth, buttonHeight, 'k');
-  happyButton.color = "lightyellow";
+  happyButton.color = "#ffe957";
   happyButton.text = "Happy";
 
   madButton = new Sprite(windowWidth/5 * 2, windowHeight - windowHeight/8, buttonWidth, buttonHeight, 'k');
-  madButton.color = "red";
+  madButton.color = "#ed544c";
   madButton.text = "Mad";
 
   sadButton = new Sprite(windowWidth/5 * 3, windowHeight - windowHeight/8, buttonWidth, buttonHeight, 'k');
@@ -151,12 +245,24 @@ function makeButtons() {
   sadButton.text = "Sad";
 
   partyButton = new Sprite(windowWidth/5 * 4, windowHeight - windowHeight/8, buttonWidth, buttonHeight, 'k');
-  partyButton.color = "plum";
+  partyButton.color = "#eec7ff";
   partyButton.text = "Party";
+
+  //mute button for music
+  muteButton = new Sprite (windowWidth - windowWidth/8, windowHeight/16, buttonWidth, buttonHeight, 'k');
+  muteButton.color = 'white';
+  muteButton.text = "Mute";
 }
 
 function backgroundText() {
   //text above buttons
   textAlign(CENTER);
   text('How do you feel today?', windowWidth/2, windowHeight - windowHeight/4);
+}
+
+function muteMusic() {
+  happyMusic.stop();
+  madMusic.stop();
+  sadMusic.stop();
+  partyMusic.stop();
 }
